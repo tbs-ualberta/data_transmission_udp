c318f5d6993626a35b55c15ec140da7926d0755a
J-Fong <jmfong@ualberta.ca>
2018-02-20 00:29:54 -0700

    Fixed possible memory leak, added support for multiple markers, cleaned code

--- MicronTracker_udp_streaming_server/MicronTracker_udp_streaming_server.cpp
+++ MicronTracker_udp_streaming_server/MicronTracker_udp_streaming_server.cpp
@@ -21,15 +21,11 @@ the closing/freeing code into whatever you use the motion tracker for.
 #include <MTC.h> // MTC.h need to be in the local directory or include path
 
 // Macro to check for and report MTC usage errors.
-#define MTC(func) {int r = func;  }; 
+#define MTC(func) { int r = func; }; 
 
 #ifdef WIN32
 	int getMTHome(char *sMTHome, int size); // Forward declaration
 #endif
-
-float _X[2][2], _Y[2][2], _Z[2][2]; // First index is current/past position,
-// second index is current/past time
-float _Vx[2], _Vy[2], _Vz[2]; // First index is velocity, second is time
 
 
 int _tmain(int argc, _TCHAR* argv[], char* envp[])
@@ -83,9 +79,10 @@ int _tmain(int argc, _TCHAR* argv[], char* envp[])
 	strcpy_s(ip_remote_scp, tmp.c_str());
 	short port_remote_ss = (short)config.pInt("port_remote");
 	short port_local_ss = (short)config.pInt("port_local");
-	tmp = config.pString("name_marker").c_str();
-	char marker_name_scp[1024];
-	strcpy_s(marker_name_scp, tmp.c_str());
+	tmp = config.pString("names_marker").c_str();
+	char marker_names_scp[1024];
+	strcpy_s(marker_names_scp, tmp.c_str());
+	short num_markers = (short)config.pInt("num_markers");
 
 	printf("\nLocal IP address: %s:%d", ip_local_scp, port_local_ss);
 	printf("\nRemote IP address: %s:%d", ip_remote_scp, port_remote_ss);
@@ -109,7 +106,7 @@ int _tmain(int argc, _TCHAR* argv[], char* envp[])
 		//return 0;
 	}
 	mtHandle CurrCamera, IdentifyingCamera;
-	int		CurrCameraSerialNum;
+	int	CurrCameraSerialNum;
 	// Obtain a handle to the first/only camera in the array
 	MTC(Cameras_ItemGet(0, &CurrCamera));
 	// Obtain its serial number
@@ -129,19 +126,31 @@ int _tmain(int argc, _TCHAR* argv[], char* envp[])
 	mtHandle PoseXf = Xform3D_New();
 
 	//-=-=-=- DATA CAPTURE AND DEPOSITING -=-=-=-//
-	int i = 0;
 	//fprintf(stderr, "\nPress any key to quit.\n");
 	Sleep(3000);
 
-	int k = 0;
+	const short len_data_ss = 3;
+	double data[len_data_ss * num_markers];
+	for (int i = 0; i < len_data_ss * num_markers) {
+		data[i] = 0.0;
+	}
+	
+	int i = 0;
+
+	clock_t start;
+	clock_t end;
+	float seconds;
+	float sample_time = (1 / (float)20);
+
 	printf("\nStreaming to %s:%d... \nPress any key to quit.", ip_remote_scp, port_remote_ss);
-	while ((k == 0) && (!comm_error)){
-		clock_t start = clock();
-		k = _kbhit();
-		i++;
+	while ((!_kbhit()) && (!comm_error)) {
+		start = clock();
 		MTC(Cameras_GrabFrame(NULL)); // Grab a frame (all cameras together)
 		MTC(Markers_ProcessFrame(NULL)); // Process the frame(s) to obtain measurements
-		if (i < 40) continue; // The first 20 frames are auto-adjustment frames, and would be ignored here
+		if (i < 40) {
+			i++;
+			continue; // The first 20 frames are auto-adjustment frames, and would be ignored here
+		}
 		// Here, MTC internally maintains the measurement results.
 		// Those results can be accessed until the next call to Markers_ProcessFrame, when they
 		// are updated to reflect the next frame's content.
@@ -181,53 +190,35 @@ int _tmain(int argc, _TCHAR* argv[], char* envp[])
 				// (and slightly more accurage) access the rotation as a 3x3 rotation matrix.
 				MTC(Xform3D_RotAnglesDegsGet(PoseXf, &Angle[0], &Angle[1], &Angle[2]));
 				MTC(Xform3D_HazardCodeGet(PoseXf, &Hazard));
-				// Print the report
-				// printf(">> %s at (%0.2f, %0.2f, %0.2f), rotation (degs): (%0.1f, %0.1f, %0.1f) %s\n",
-				// MarkerName,
-				_X[0][0] = Position[0];
-				_Y[0][0] = Position[1];
-				_Z[0][0] = Position[2];
-				_X[0][1] = _Y[0][1] = _Z[0][1] = i;
 
-				if (i < 40) {
-					_Vx[0] = _Vy[0] = _Vz[0] = 0;
-					_Vx[1] = _Vy[1] = _Vz[1] = i;
-				}
-				else if (i % 5 == 0) {
-					_Vx[0] = 20 * (_X[0][0] - _X[1][0]) / (_X[0][1] - _X[1][1]);
-					_Vy[0] = 20 * (_Y[0][0] - _Y[1][0]) / (_X[0][1] - _X[1][1]);
-					_Vz[0] = 20 * (_Z[0][0] - _Z[1][0]) / (_X[0][1] - _X[1][1]);
-					_Vx[1] = _Vy[1] = _Vz[1] = i;
-
-					// Send position data if identified marker matches specified one
-					if (!strcmp(MarkerName, marker_name_scp)) {
-						const short len_data_ss = 3;
-						double data[len_data_ss];
-						data[0] = _X[0][0];
-						data[1] = _Y[0][0];
-						data[2] = _Z[0][0];
-						comm_error = transmission.send(data, len_data_ss);
-
+				// Send position data if identified marker matches specified one
+				char marker_names_tmp[1024];
+				char marker_names_tok;
+				strcpy_s(marker_names_tmp, marker_names_scp);
+				marker_names_tok = strtok(marker_names_tmp, " ,");
+				int ind_marker = 0;
+				while (marker_names_tok != NULL) {
+					if (!strcmp(MarkerName, marker_names_tok)) {
+						for (int k = 0; k < len_data_ss; k++) {
+							data[k + ind_marker * len_data_ss] = Position[k];
+						}
+						break;
 					}
+					ind_marker++;
+					marker_names_tok = strtok(NULL, " ,");
 				}
-				// Update 'past' positions
-				_X[1][0] = _X[0][0];
-				_Y[1][0] = _Y[0][0];
-				_Z[1][0] = _Z[0][0];
 			}
 		}
-		_X[1][1] = _Y[1][1] = _Z[1][1] = i;
+		comm_error = transmission.send(data, num_markers * len_data_ss);
 
-		clock_t end = clock();
-		float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-		float sample_time = (1 / (double)20);
+		end = clock();
+		seconds = (float)(end - start) / CLOCKS_PER_SEC;
 		if (seconds > sample_time) 
 			printf("\nSample time > %2.4f s: %2.4f", sample_time, seconds);
 	}
 
 	//-=-=-=- CLEANUP -=-=-=-//
 	// Free up all resources taken
-	//sqlite3_close(db);
 
 	Collection_Free(IdentifiedMarkers);
 	Xform3D_Free(PoseXf);
